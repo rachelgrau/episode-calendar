77ab1fc84567e3d266a03623546267431b25147b
Rachel Grau <rgrau@stanford.edu>
2015-01-23 16:46:11 -0800

    Initial Commit

---
+++ EpisodeCalendar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EpisodeCalendar
+//
+//  Created by Rachel on 23/01/2015.
+//  Copyright (c) 2015 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EpisodeCalendar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EpisodeCalendar
+//
+//  Created by Rachel on 23/01/2015.
+//  Copyright (c) 2015 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
