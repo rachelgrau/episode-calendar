0a036f2b5a6a8eb00c7d6d68868929a50aa1488e
rachelgrau <rgrau@stanford.edu>
2015-01-27 01:13:09 -0800

    Display month name and year on calendar, change when you click on arrows

---
+++ EpisodeCalendar/EpisodeCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  EpisodeCalendarViewController.h
+//  EpisodeCalendar
+//
+//  Created by Rachel on 27/01/2015.
+//  Copyright (c) 2015 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EpisodeCalendarViewController : UIViewController
+@property NSDate *dateToDisplay;
+@end
---
+++ EpisodeCalendar/EpisodeCalendarViewController.m
@@ -0,0 +1,84 @@
+//
+//  EpisodeCalendarViewController.m
+//  EpisodeCalendar
+//
+//  Created by Rachel on 27/01/2015.
+//  Copyright (c) 2015 Rachel. All rights reserved.
+//
+
+#import "EpisodeCalendarViewController.h"
+
+@interface EpisodeCalendarViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *monthLabel;
+@property (strong, nonatomic) IBOutlet UILabel *yearLabel;
+@end
+
+@implementation EpisodeCalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+/* Given a number of months (probably 1 or -1), returns an NSDate * with adding that # of months from the date currently being displayed.
+   For example, if we are currently displaying January 22, 2015, and you call getDate:-1, will return an NSDate * with the date December 22, 2014.*/
+- (NSDate *)getDate:(int)monthsAway {
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setMonth:monthsAway];
+    NSDate *ret = [gregorian dateByAddingComponents:offsetComponents toDate:self.dateToDisplay options:0];
+    return ret;
+}
+
+- (IBAction)leftArrowPressed:(id)sender {
+    self.dateToDisplay = [self getDate:-1];
+    [self viewDidLoad];
+}
+
+- (IBAction)rightArrowPressed:(id)sender {
+    self.dateToDisplay = [self getDate:1];
+    [self viewDidLoad];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    if (!self.dateToDisplay) {
+        self.dateToDisplay = [NSDate date];
+    }
+    
+    /* Set up month label */
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"MMMM"];
+    self.monthLabel.text = [dateFormatter stringFromDate:self.dateToDisplay];
+    self.monthLabel.textAlignment = NSTextAlignmentCenter;
+    [self.monthLabel setFont:[UIFont systemFontOfSize:22]];
+    
+    /*Set up year label */
+    [dateFormatter setDateFormat:@"YYYY"];
+    self.yearLabel.text = [dateFormatter stringFromDate:self.dateToDisplay];
+    self.yearLabel.textAlignment = NSTextAlignmentCenter;
+    [self.yearLabel setFont:[UIFont systemFontOfSize:16]];
+    self.yearLabel.textColor = [UIColor grayColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
